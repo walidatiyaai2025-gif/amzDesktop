7aa43052b57353b988604536204c2e94518673a9
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-23 10:53:48 +0300

    Fix Flutter analyzer warning

--- remotescreen-v21/mobile/lib/main.dart
+++ remotescreen-v21/mobile/lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:just_audio/just_audio.dart';
